f15e200caa0c4843d1e7c3cdbc82b46d736b7833
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-08-17 18:44:13 +0600

    feat: Add footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div>
-      Header!!
+          <nav className='py-4 flex justify-between items-center'>
+              <Link>
+                  <h1 className='font-bold text-5xl text-yellow-500'>CareerHub</h1>
+              </Link>
+              <button>JobSeeker</button>
+              <button>Employers</button>
+              <button>Admin</button>
+      </nav>
     </div>
   )
 }
