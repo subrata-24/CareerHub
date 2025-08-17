058d8d1ec6d323abce1d28b2a3fd2cd55167fc41
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-08-17 19:10:28 +0600

    feat: Add background video in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import bgVideo from "../assets/bg-video.mp4"; // put your video in /src/assets
 
-const Home = () => {
+const BackgroundVideo = () => {
   return (
-    <div>
-      Hiii!!
-    </div>
-  )
-}
+    <div className="relative w-full h-screen">
+      {/* Video Background */}
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover -z-10"
+      >
+        <source src={bgVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-export default Home
+      {/* Content */}
+      <div className="relative flex items-center justify-center h-full text-white text-4xl font-bold">
+        Welcome to My Page 🚀
+      </div>
+    </div>
+  );
+};
+
+export default BackgroundVideo;
